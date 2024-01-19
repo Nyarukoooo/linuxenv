41b18ddb0f2ca75e4f7b08a767e09d70efce7e3e
Nyarukoooo <xiaosonw@uci.edu>
2024-01-19 12:39:52 +0800

    aliase update

--- zsh_backup/alias.zsh
+++ zsh_backup/alias.zsh
@@ -1,5 +1,5 @@
 alias updatedb="sudo /usr/libexec/locate.updatedb"
 alias PATH="echo $PATH | tr ':' '\n'"
 alias wk="cd ~/wxsbin"
-alias el="eza -lh"
-alias ea="eza -lha"
+alias el="eza -lh --git"
+alias ea="eza -lha --git"
